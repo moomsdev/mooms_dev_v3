f34f4f37c1ed3fbb29084cc97b88727b9912c6e5
anhd-aiot <anhd@aiot-global.com>
2025-02-24 13:23:07 +0700

    update - theme/functions

--- theme/functions.php
+++ theme/functions.php
@@ -213,9 +213,7 @@ function tinymce_allow_unsafe_link_target($mceInit)
     return $mceInit;
 }
 
-
 new \App\PostTypes\blog();
-
 
 function my_theme_enqueue_scripts() {
     // Đăng ký script của bạn
@@ -225,55 +223,3 @@ function my_theme_enqueue_scripts() {
     wp_localize_script('my-theme-script', 'ajaxurl', admin_url('admin-ajax.php'));
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
-
-
-// function sendContactForm() {
-//     // Kiểm tra nonce để bảo mật
-//     if (!check_ajax_referer('send_contact_form', '_token', false)) {
-//         wp_send_json_error(['message' => __('Token mistake.')]);
-//     }
-
-//     // Kiểm tra các trường bắt buộc
-//     if (empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['email']) || empty($_POST['phone_number']) || empty($_POST['message'])) {
-//         wp_send_json_error(['message' => __('Please fill in all required fields.', 'gaumap')]);
-//     }
-
-//     // Lấy thông tin từ form
-//     $first_name = sanitize_text_field($_POST['first_name']);
-//     $last_name = sanitize_text_field($_POST['last_name']);
-//     $email = sanitize_email($_POST['email']);
-//     $phone_number = sanitize_text_field($_POST['phone_number']);
-//     $message = sanitize_textarea_field($_POST['message']);
-    
-//     // Lấy thông tin blog
-//     $blogName = get_bloginfo('name');
-//     $blogUrl = get_bloginfo('url');
-
-//     // Nội dung email
-//     $html = sprintf(
-//         '<p>Send from: %s %s (%s)</p><p>Contact phone number: %s</p><p>Contact message:</p><p>%s</p>',
-//         esc_html($first_name),
-//         esc_html($last_name),
-//         esc_html($email),
-//         esc_html($phone_number),
-//         esc_html($message)
-//     );
-
-//     // Thiết lập header
-//     $headers = [
-//         'Content-Type: text/html; charset=UTF-8',
-//         'Reply-To: ' . esc_html($first_name . ' ' . $last_name) . ' <' . sanitize_email($email) . '>',
-//     ];
-
-//     // Gửi email đến quản trị viên
-//     $success = wp_mail(get_option('admin_email'), $blogName . ': New Contact Form Submission', $html, $headers);
-
-//     // Kiểm tra kết quả gửi email và phản hồi JSON
-//     if ($success) {
-//         wp_send_json_success(['message' => __('Your request has been successfully submitted.', 'gaumap')]);
-//     } else {
-//         // Ghi lại log nếu gửi email thất bại
-//         error_log('Email failed to send.');
-//         wp_send_json_error(['message' => __('An error occurred. Please try again later.', 'gaumap')]);
-//     }
-// }
